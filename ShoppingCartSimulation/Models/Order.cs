fffbe85f60b3b0361e73c9d6d833d02635171cd6
Kazioslaw <Kamil_d13@wp.pl>
2024-12-10 14:25:16 +0100

    Added basic models.

---
+++ ShoppingCartSimulation/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace ShoppingCartSimulation.Models
+{
+	class Order
+	{
+		private static int _nextID = 1;
+		public Order()
+		{
+			ID = _nextID++;
+			OrderDate = DateTime.Now;
+		}
+		public int ID { get; private set; }
+		public DateTime OrderDate { get; private set; }
+		public ICollection<OrderItem> OrderItems { get; private set; }
+	}
+}
---
+++ ShoppingCartSimulation/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ShoppingCartSimulation.Models
+{
+	class Product
+	{
+		public Product(int id, string name, decimal price)
+		{
+			ID = id;
+			Name = name;
+			Price = price;
+		}
+
+		public int ID { get; private set; }
+		public string Name { get; private set; }
+		public decimal Price { get; private set; }
+	}
+}
